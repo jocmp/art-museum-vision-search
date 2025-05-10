42c5498333f37202eb37dbf3bb4093aa9fac167d
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-05-10 17:28:33 -0500

    Try minimal setup

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
-from fastapi import UploadFile
-from fastapi.responses import JSONResponse
+# from fastapi import UploadFile
+# from fastapi.responses import JSONResponse
 
-from app.search.related_images import search_related_images
+# from app.search.related_images import search_related_images
 
 
 app = FastAPI()
 
 
-@app.post("/search")
-async def search(image: UploadFile):
-    try:
-        result = await search_related_images(image)
+# @app.post("/search")
+# async def search(image: UploadFile):
+#     try:
+#         result = await search_related_images(image)
 
-        return JSONResponse(content={"results": result})
-    except Exception as e:
-        return JSONResponse(
-            status_code=500,
-            content={"error": f"Error processing image: {str(e)}"}
-        )
+#         return JSONResponse(content={"results": result})
+#     except Exception as e:
+#         return JSONResponse(
+#             status_code=500,
+#             content={"error": f"Error processing image: {str(e)}"}
+#         )
 
 @app.get("/health")
 async def health():
