046ac24119256aab83e0c676b8c43012111a71b0
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-06-13 19:36:30 -0500

    Move engine out of db_session

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -5,13 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 DB_URL = os.environ.get("DATABASE_URL")
+engine = create_engine(DB_URL)
 
 
 @contextmanager
 def db_session():
     """ Creates a context with an open SQLAlchemy session.
     """
-    engine = create_engine(DB_URL)
     connection = engine.connect()
     db_session = scoped_session(sessionmaker(autoflush=True, bind=engine))
     yield db_session
